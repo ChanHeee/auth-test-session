0c7100bd18d75d331b8d0f28587994bd77c78d13
박찬희 <pch0989@gmail.com>
2022-03-10 09:52:23 +0900

    feat : DB 모델 생성 및 연결
    - user 모델 생성
    - mysql 커넥트

---
+++ backend/config/config.js
@@ -0,0 +1,21 @@
+require("dotenv").config()
+
+const env = process.env
+
+const DEV = {
+  username: "root",
+  password: env.MYSQL_PASSWORD,
+  database: "auth-app",
+  host: "127.0.0.1",
+  dialect: "mysql",
+}
+
+const PROD = {
+  username: "root",
+  password: env.MYSQL_PASSWORD,
+  database: "auth-app",
+  host: "127.0.0.1",
+  dialect: "mysql",
+}
+
+module.exports = { DEV, PROD }
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const morgan = require("morgan")
 const dotenv = require("dotenv")
 const passport = require("passport")
 const { notFound, errorHandler } = require("./middleware/errorMiddleware")
+const { sequelize } = require("./models/index")
 
 //* @import Routers
 const indexRouter = require("./routes/indexRoutes")
@@ -13,6 +14,12 @@ const indexRouter = require("./routes/indexRoutes")
 dotenv.config()
 
 const app = express()
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("데이터베이스 연결 성공"))
+  .catch((err) => {
+    console.error(err)
+  })
 
 app.use(morgan("dev"))
 app.use(express.json())
